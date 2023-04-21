286f05b00d366a453c48b2113ea253e2ccf3537d
DrHunterSThompson <110724889+DrHunterSThompson@users.noreply.github.com>
2023-04-22 01:45:11 +0300

    lab5

--- lab/5/js/script.js
+++ lab/5/js/script.js
@@ -7,7 +7,6 @@ window.onload = function () {
 document.querySelector("div#datetime").onmouseover = function () {
     intervalID = setInterval(showTime, 1000);
 };
-
 document.querySelector("div#datetime").onmouseout = function () {
     clearInterval(intervalID);
     showDate();
@@ -16,7 +15,6 @@ document.querySelector("div#datetime").onmouseout = function () {
 function showDate() {
     document.querySelector("div#datetime").innerHTML = new Date().toDateString();
 }
-
 function showTime() {
     document.querySelector("div#datetime").innerHTML = new Date().toLocaleTimeString();
 }
@@ -28,28 +26,24 @@ document.querySelector("#img1").onmouseover = function () {
 document.querySelector("#img1").onmouseout = function () {
     document.images['img1'].src = "images/1/image.jpg";
 }
-
 document.querySelector("#img2").onmouseover = function () {
     document.images['img2'].style = "filter: blur(0px);"
 }
 document.querySelector("#img2").onmouseout = function () {
-    document.images['img2'].style = "filter: blur(5px);"
+    document.images['img2'].style = "filter: blur(3px);"
 }
-
 document.querySelector("#img3").onmouseover = function () {
     document.images['img3'].style = "filter: grayscale(0%);"
 }
 document.querySelector("#img3").onmouseout = function () {
     document.images['img3'].style = "filter: grayscale(100%);"
 }
-
 document.querySelector("#img4").onmouseover = function () {
     document.images['img4'].style = "filter: opacity(1);"
 }
 document.querySelector("#img4").onmouseout = function () {
     document.images['img4'].style = "filter: opacity(.5);"
 }
-
 document.querySelector("#img5").onmouseover = function () {
     document.images['img5'].style = "filter: brightness(1.2);"
 }
